4a16590fd99b4daeb96851b7b7059b6367407516
Faizan <01kinggaming@gmail.com>
2026-06-02 21:13:00 +0530

    Day 4 project update

--- Day_4_Rock Paper Scissors.py
+++ Day_4_Rock Paper Scissors.py
@@ -1,36 +1,66 @@
 import random
-print("Welcome to Rock Paper Scissors Game.")
+print("Welcome to Rock, Paper or Scissor Game.")
 
-computer = ["Rock","Paper","Scissors"]
+Computers = [0,1,2]
+poster =[
+        '''
+            _______
+        ---'    ___)_
+                _____)
+                _____)
+                (___)
+        ---.___(__)
+        '''
+        ,'''
+            ______
+        ---'   ___)_____
+                   _____)
+                    _____)
+                    ___)
+        ---._________)
+        '''
+        ,'''
+            _______
+        ---'    ___)___
+                  _____)
+                ________)
+                (___)
+        ---.___(__)
+        '''
+]
+
+
 num = random.randint(0,2)
-user = input("Enter Your Choice Rock, Paper or Scissor\n").capitalize()
-if user == "Rock" and computer[num]== "Rock":
-    print(f"Computer : {computer[num]}")
+user = int(input("Enter Your Choice 0 for 0, 1 for 1 and 2 for 2: "))
+print(poster[user])
+if user == num:
+    print("Computer Chose:")
+    print(poster[num])
     print("Drow")
-elif user == "Rock" and computer[num]== "Paper":
-    print(f"Computer : {computer[num]}")
-    print("Lose")
-elif user == "Rock" and computer[num]== "Scissor":
-    print(f"Computer : {computer[num]}")
-    print("Win!")
-elif user == "Paper" and computer[num]== "Rock":
-    print(f"Computer : {computer[num]}")
-    print("Win!")
-elif user == "Paper" and computer[num]== "Paper":
-    print(f"Computer : {computer[num]}")
-    print("Drow")
-elif user == "Paper" and computer[num]== "Scissor":
-    print(f"Computer : {computer[num]}")
-    print("Lose!")
-elif user == "Scissor" and computer[num]== "Rock":
-    print(f"Computer : {computer[num]}")
-    print("Lose")
-elif user == "Scissor" and computer[num]== "Paper":
-    print(f"Computer : {computer[num]}")
-    print("Win!")
-elif user == "Scissor" and computer[num]== "Scissor":
-    print(f"Computer : {computer[num]}")
-    print("Drow!")
+elif user == 0 and num== 1:
+    print("Computer Chose:")
+    print(poster[num])
+    print("You Lose")
+elif user == 0 and num== 2:
+    print("Computer Chose:")
+    print(poster[num])
+    print("You Win!")
+elif user == 1 and num== 0:
+    print("Computer Chose:")
+    print(poster[num])
+    print("You Win!")
+elif user == 1 and num== 2:
+    print("Computer Chose:")
+    print(poster[num])
+    print("You Lose!")
+elif user == 2 and num== 0:
+    print("Computer Chose:")
+    print(poster[num])
+    print("You Lose")
+elif user == 2 and num== 1:
+    print("Computer Chose:")
+    print(poster[num])
+    print("You Win!")
 
-print(f"final : {computer[num]}")
+
 
